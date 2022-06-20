22e5c30e69dd87c84fbbf532ec50a6050579231d
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-06-20 23:03:47 +0500

    wip:working on tours component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
-const url = 'https://course-api.com/react-tours-project'
+import React from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+const url = "https://course-api.com/react-tours-project";
 function App() {
   const [tour, settour] = React.useState([]);
   const [loading, setloading] = React.useState(false);
-  const fetchtours=async ()=>{
-    setloading(true)
+  const fetchtours = async () => {
+    setloading(true);
     try {
-      const response= await fetch(url);
-      const data=await response.json()
-      setloading(false)
-      settour(data)
-    } catch (error) { 
-      setloading(false)
-      console.log(error)
+      const response = await fetch(url);
+      const data = await response.json();
+      setloading(false);
+      settour(data);
+    } catch (error) {
+      console.log(error);
+      setloading(false);
     }
-  }
-  React.useEffect(()=>{
-    fetchtours()
-  },[])
-  if(loading){
-    return  <Loading /> 
+  };
+  React.useEffect(() => {
+    fetchtours();
+  }, []);
+  if (loading) {
+    return <Loading />;
   }
   return (
     <main>
       <Tours tour={tour} />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Tours.js
+++ src/Tours.js
@@ -1,11 +1,29 @@
 import React from "react";
 import Tour from "./Tour";
-const Tours = () => {
+const Tours = ({tour}) => {
+ console.log(tour);
   return (
     <section>
       <div className="title">
         <h2>our tours</h2>
         <div className="underline" />
+      </div>
+      <div>
+        {
+          tour?.map((item,index)=>(
+            <article className="single-tour" key={index}>
+              <img src={item.image} alt={item.name} />
+              <footer>
+                <div className="tour-info">
+                <h4>{item.name}</h4>
+                <h4>{item.price}</h4>
+                </div>
+                <p>{item.info}</p>
+                <button className="delete-btn">not interested</button>
+              </footer>
+            </article>
+          ))
+        }
       </div>
     </section>
   );
