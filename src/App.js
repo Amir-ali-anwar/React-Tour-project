d345e4f2528a74dd54ec5bf79fd69478f787ecf1
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-06-20 22:47:16 +0500

    fetch tours

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [tour, settour] = React.useState([]);
+  const [loading, setloading] = React.useState(false);
+  const fetchtours=async ()=>{
+    setloading(true)
+    try {
+      const response= await fetch(url);
+      const data=await response.json()
+      setloading(false)
+      settour(data)
+    } catch (error) { 
+      setloading(false)
+      console.log(error)
+    }
+  }
+  React.useEffect(()=>{
+    fetchtours()
+  },[])
+  if(loading){
+    return  <Loading /> 
+  }
+  return (
+    <main>
+      <Tours tour={tour} />
+    </main>
+  )
 }
 
 export default App
